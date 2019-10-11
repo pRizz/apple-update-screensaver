c33d16b54584636d73fb155046c1d1f09ba4a386
Peter Ryszkiewicz <pRizz@users.noreply.github.com>
2019-10-11 00:23:51 -0500

    Created basic loading bar and basic update text

--- src/apple_update_screensaver/core.cljs
+++ src/apple_update_screensaver/core.cljs
@@ -5,6 +5,29 @@
 ;; -------------------------
 ;; Views
 
+(def loadingBarPath
+  [:path {:d "M 0 0 h 100 c 10 0 10 15 0 15 h -100 c -10 0 -10 -15 0 -15 Z"}])
+
+(defn loadingBarCmp [percentFilled]
+  [:svg {:viewBox "0 0 200 200"}
+   [:clipPath {:id "loadingBarClipPath"} loadingBarPath]
+   [:path {:id "loadingBarPath"
+           :d "M 0 0 h 100 c 10 0 10 15 0 15 h -100 c -10 0 -10 -15 0 -15 Z"}]
+   [:circle {:cx "0" :cy "50" :r "1000" :id "mycircle"}]
+   [:g {:id "loadingBarContent"}
+    [:use {:href "#loadingBarPath"
+           :fill "gray"
+           :stroke "white"}]
+    [:rect {:id "filledLoadingBar"
+            :fill "white"
+            :x (+ -210 (* percentFilled 0.01 120))
+            :y 0
+            :width 200
+            :height 20}]]
+   [:use {:clip-path "url(#loadingBarClipPath)"
+          :href "#loadingBarContent"
+          :y 50}]])
+
 (defn home-page []
   [:div {:style
          {:display "flex"
@@ -12,12 +35,24 @@
           :height "100%"
           :width "100%"
           :align-items "center"
+          :flex-direction "column"
           :justify-content "center"}}
-;   [:div {:style {:color "red"}}
-;    "testing"]
    [:img {:src "assets/Apple_logo_white.svg"
-          :style
-          {:width "10%"}}]])
+          :style {:width "10%"}}]
+   [:div {:style
+          {:borderRadius 1000
+           :backgroundColor "#444"
+           :width "20%"
+           :margin-top 30}}
+    " a"]
+   (loadingBarCmp 100)
+   [:div
+    {:style
+     {:color "white"
+      :font-weight "bold"
+      :font-size 12
+      :margin-top 12}}
+    "Installing Software Update: About 42 minutes"]])
 
 ;; -------------------------
 ;; Initialize app
